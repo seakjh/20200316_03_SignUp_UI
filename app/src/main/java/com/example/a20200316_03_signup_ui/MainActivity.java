b8af1e4731f19fc36bc596067a79faf74c92ed5a
seakjh <skykjh0513@gmail.com>
2020-03-17 10:20:24 +0900

    Initial commit

---
+++ app/src/main/java/com/example/a20200316_03_signup_ui/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.a20200316_03_signup_ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
