07df4dda81740bae923637d9f0ffc3ae03864a0d
seakjh <skykjh0513@gmail.com>
2020-03-17 13:04:15 +0900

    toast연습문제

--- app/src/main/java/com/example/a20200316_03_signup_ui/MainActivity.java
+++ app/src/main/java/com/example/a20200316_03_signup_ui/MainActivity.java
@@ -6,11 +6,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     Button loginBtn = null;
     TextView findPwtxt = null;
+    TextView signUpTxt = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,13 +20,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         loginBtn = findViewById(R.id.loginBtn);
+        findPwtxt = findViewById(R.id.findPwtxt);
+        signUpTxt = findViewById(R.id.signUpTxt);
+
+
 //        로그인 버튼의 글자를 회원가입으로 바꿈
         loginBtn.setText("회원가입");
-
 //        비밀번호 찾기를 비번 찾기로 바꾸어보자
 //        id는 findPwtxt
-        findPwtxt = findViewById(R.id.findPwtxt);
         findPwtxt.setText("비번 찾기");
+
 
 //       로그인버튼이 눌리면 => 비번찾기 버튼을 aaa로 변경
 
@@ -51,6 +56,16 @@ public class MainActivity extends AppCompatActivity {
             public boolean onLongClick(View v) {
                 loginBtn.setText("로그인");
                 return true;   // 손을 뗏을때 OnClick 실행을 막을건데?:true, 막지않을거야:false
+            }
+        });
+
+
+        signUpTxt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(MainActivity.this, "회원가입 화면으로 이동합니다", Toast.LENGTH_SHORT).show();
+
             }
         });
 
